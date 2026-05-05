fd630e965429199be413f3793c709368cb2f135a
Milan Stojanov <milan@mageclass.studio>
2026-05-05 15:28:03 +0200

    Improves handling partially successful requests (10 ASINs, 1 invalid, 9 valid)

--- operations.go
+++ operations.go
@@ -9,7 +9,7 @@ import (
 func (c *Client) GetItems(ctx context.Context, params *api.GetItemsParams) (*api.GetItemsResponse, error) {
 	response := api.GetItemsResponse{}
 	if err := c.executeOperation(ctx, api.OperationGetItems, params, &response); err != nil {
-		return nil, err
+		return &response, err
 	}
 	return &response, nil
 }
@@ -17,7 +17,7 @@ func (c *Client) GetItems(ctx context.Context, params *api.GetItemsParams) (*api
 func (c *Client) GetVariations(ctx context.Context, params *api.GetVariationsParams) (*api.GetVariationsResponse, error) {
 	response := api.GetVariationsResponse{}
 	if err := c.executeOperation(ctx, api.OperationGetVariations, params, &response); err != nil {
-		return nil, err
+		return &response, err
 	}
 	return &response, nil
 }
